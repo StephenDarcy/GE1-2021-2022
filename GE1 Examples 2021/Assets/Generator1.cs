344db9e93e8dbdc9c2c1bcf03f08b9067016816c
Stephen Darcy <stephendarcy123@gmail.com>
2021-09-26 23:16:54 +0100

    made some dodecahedrons spin

--- GE1 Examples 2021/Assets/Generator1.cs
+++ GE1 Examples 2021/Assets/Generator1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CircleSpawner : MonoBehaviour
+public class Generator1 : MonoBehaviour
 {
     public GameObject prefab;
     public float radius = 20;
@@ -11,26 +11,31 @@ public class CircleSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float theta  = (Mathf.PI * 2.0f) / (float) points;
-        for (int i = 0 ; i < points ; i ++)
+        float theta = (Mathf.PI * 2.0f) / (float)points;
+        for (int j = 0; j < radius; j++)
         {
-            float angle = theta * i;
-            Vector3 position = new Vector3(
-                Mathf.Sin(angle) * radius,
-                Mathf.Cos(angle) * radius,
-                0
-            );
-            Debug.Log(angle);
-            Debug.Log(position);
-            GameObject obj = GameObject.Instantiate(prefab, position, Quaternion.identity);
-            obj.transform.parent = transform;
+            for (int i = 0; i < points; i++)
+            {
+                float angle = theta * i;
+                Vector3 position = new Vector3(
+                    Mathf.Sin(angle) * radius,
+                    Mathf.Cos(angle) * radius,
+                    0
+                );
+                Debug.Log(angle);
+                Debug.Log(position);
+                GameObject obj = GameObject.Instantiate(prefab, position, Quaternion.identity);
+                obj.transform.parent = transform;
+            }
+
         }
-        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- GE1 Examples 2021/Assets/RotateMe.cs
+++ GE1 Examples 2021/Assets/RotateMe.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class RotateMe : MonoBehaviour
 {
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        transform.Rotate(0, 5, 0);
     }
 }
