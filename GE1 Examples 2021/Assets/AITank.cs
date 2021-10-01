d5d96691ea547a894b3e40651997e8f458005a98
Stephen Darcy <stephendarcy123@gmail.com>
2021-10-01 16:57:33 +0100

    part 1,2,3

--- GE1 Examples 2021/Assets/AITank.cs
+++ GE1 Examples 2021/Assets/AITank.cs
@@ -3,40 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class AITank : MonoBehaviour {
+public class AITank : MonoBehaviour
+{
 
     public float radius = 10;
     public int numWaypoints = 5;
     public int current = 0;
     List<Vector3> waypoints = new List<Vector3>();
     public float speed = 10;
-    public Transform player;    
+    public Transform player;
 
     public void OnDrawGizmos()
     {
         if (!Application.isPlaying)
         {
             // Task 1
-            // Put code here to draw the gizmos
-            // Use sin and cos to calculate the positions of the waypoints 
-            // You can draw gizmos using
-            // Gizmos.color = Color.green;
-            // Gizmos.DrawWireSphere(pos, 1);
+            float theta = (Mathf.PI * 2.0f) / ((float)numWaypoints);
+
+            for (int i = 0; i < numWaypoints; i++)
+            {
+                float angle = theta * i;
+
+                float x = Mathf.Sin(angle) * radius;
+                float y = Mathf.Cos(angle) * radius;
+
+                Vector3 pos = new Vector3(x, 0, y);
+
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireSphere(pos, 1);
+            }
+
         }
     }
 
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         // Task 2
-        // Put code here to calculate the waypoints in a loop and 
-        // Add them to the waypoints List
+        float theta = (Mathf.PI * 2.0f) / ((float)numWaypoints);
+
+        for (int i = 0; i < numWaypoints; i++)
+        {
+            float angle = theta * i;
+
+            float x = Mathf.Sin(angle) * radius;
+            float y = Mathf.Cos(angle) * radius;
+
+            Vector3 pos = new Vector3(x, 0, y);
+
+            pos = transform.TransformPoint(pos);
+
+            waypoints.Add(pos);
+
+        }
+
+
+
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         // Task 3
         // Put code here to move the tank towards the next waypoint
         // When the tank reaches a waypoint you should advance to the next one
+        Vector3 distance = waypoints[current] - transform.position;
+
+        if (distance.magnitude < 1)
+        {
+            current = (current + 1) % waypoints.Count;
+        }
+
+        distance.Normalize();
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(distance), Time.deltaTime * 5);
+        transform.Translate(distance * speed * Time.deltaTime, Space.World);
 
 
         // Task 4
