fdf1035e0fb0d585fca8c5d4b240c69865b49988
Stephen Darcy <stephendarcy123@gmail.com>
2021-10-01 15:40:43 +0100

    part of lab1

--- GE1 Examples 2021/Assets/Generator.cs
+++ GE1 Examples 2021/Assets/Generator.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
+    public int loops = 10;
+    public GameObject prefab;
     void Start()
     {
+        int num = 10;
+        float radius = 3;
+        float theta = (2.0f * Mathf.PI) / (float)num;
+        for (int i = 0; i < num; i++)
+        {
+            float angle = theta * i;
+            float x = Mathf.Sin(angle) * radius * 1.1f;
+            float y = Mathf.Cos(angle) * radius * 1.1f;
+            GameObject go = GameObject.Instantiate(< GameObject > (prefab));
+            go.transform.position = transform.Transform
+        }
     }
 
     // Update is called once per frame
