240131ddfd805ae350705b6d35a4344789b0932a
Stephen Darcy <stephendarcy123@gmail.com>
2021-10-08 16:54:40 +0100

    lab2 finish

--- GE1 Examples 2021/Assets/AITank.cs
+++ GE1 Examples 2021/Assets/AITank.cs
@@ -1,63 +1,114 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
 
 
-public class AITank : MonoBehaviour {
+public class AITank : MonoBehaviour
+{
 
     public float radius = 10;
     public int numWaypoints = 5;
     public int current = 0;
     public List<Vector3> waypoints = new List<Vector3>();
     public float speed = 10;
-    public Transform player;    
+    public Transform player;
+
+
 
     public void OnDrawGizmos()
     {
         if (!Application.isPlaying)
         {
-            float theta = (Mathf.PI * 2.0f) / numWaypoints;
-            for(int i = 0 ; i < numWaypoints ; i ++)
+            // Task 1
+            float theta = (Mathf.PI * 2.0f) / ((float)numWaypoints);
+
+            for (int i = 0; i < numWaypoints; i++)
             {
                 float angle = theta * i;
-                Vector3 pos = new Vector3(Mathf.Sin(angle) * radius, 0, Mathf.Cos(angle) * radius);
+
+                float x = Mathf.Sin(angle) * radius;
+                float y = Mathf.Cos(angle) * radius;
+
+                Vector3 pos = new Vector3(x, 0, y);
                 pos = transform.TransformPoint(pos);
-                Gizmos.color = Color.green;
-                Gizmos.DrawWireSphere(pos, 1); 
+
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireSphere(pos, 1);
             }
-            // Task 1
-            // Put code here to draw the gizmos
-            // Use sin and cos to calculate the positions of the waypoints 
-            // You can draw gizmos using
-            // Gizmos.color = Color.green;
-            // Gizmos.DrawWireSphere(pos, 1);
+
         }
     }
 
     // Use this for initialization
-    void Awake () {
-        float theta = (Mathf.PI * 2.0f) / numWaypoints;
-        for(int i = 0 ; i < numWaypoints ; i ++)
+    void Awake()
+    {
+        // Task 2
+        float theta = (Mathf.PI * 2.0f) / ((float)numWaypoints);
+
+        for (int i = 0; i < numWaypoints; i++)
         {
             float angle = theta * i;
-            Vector3 pos = new Vector3(Mathf.Sin(angle) * radius, 0, Mathf.Cos(angle) * radius);
+
+            float x = Mathf.Sin(angle) * radius;
+            float y = Mathf.Cos(angle) * radius;
+
+            Vector3 pos = new Vector3(x, 0, y);
+
             pos = transform.TransformPoint(pos);
-            waypoints.Add(pos); 
+
+            waypoints.Add(pos);
+
         }
+
+
+
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         // Task 3
         // Put code here to move the tank towards the next waypoint
         // When the tank reaches a waypoint you should advance to the next one
+        Vector3 distance = waypoints[current] - transform.position;
+
+        if (distance.magnitude < 1)
+        {
+            current = (current + 1) % waypoints.Count;
+        }
+
+        distance.Normalize();
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(distance), Time.deltaTime * 5);
+        transform.Translate(distance * speed * Time.deltaTime, Space.World);
 
 
         // Task 4
-        // Put code here to check if the player is in front of or behine the tank
+        // Put code here to check if the player is in front of or behind the tank
+        float distance1 = Vector3.Distance(player.position, transform.position);
+        Vector3 toTarget = player.position - transform.position;
+        toTarget = Vector3.Normalize(toTarget);
+
+        float dot = Vector3.Dot(transform.forward, toTarget);
+        float theta = Mathf.Acos(dot) * Mathf.Rad2Deg;
+        if (theta < 45)
+        {
+            GameManager.Log("In front of tank");
+        }
+        else
+        {
+            GameManager.Log("Behind tank");
+        }
         // Task 5
         // Put code here to calculate if the player is inside the field of view and in range
         // You can print stuff to the screen using:
-        GameManager.Log("Hello from th AI tank");
+        if (distance1 < 10)
+        {
+            GameManager.Log("In range");
+        }
+        else
+        {
+            GameManager.Log("Out of range");
+        }
     }
 }
